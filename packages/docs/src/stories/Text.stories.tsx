541a8790a8029479ffd9f69b0a8768231f5ad600
Matheus Castro <goca1377@gmail.com>
2022-10-29 20:29:58 -0300

    first commit

---
+++ packages/docs/src/stories/Text.stories.tsx
@@ -0,0 +1,25 @@
+import type { StoryObj, Meta } from '@storybook/react'
+import { Text, TextProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Typography/Text',
+  component: Text, 
+  args: {
+    children: `Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+    Exercitationem sapiente eum iusto similique? Ab repellendus aliquid 
+    voluptates deleniti deserunt ex, odit voluptatum ad corrupti 
+    praesentium voluptatem excepturi earum modi laborum.`,
+  }
+} as Meta<TextProps>
+
+export const Primary: StoryObj<TextProps> = {}
+
+export const CustomTag: StoryObj<TextProps> = {
+  args: {
+    children: `Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+    Exercitationem sapiente eum iusto similique? Ab repellendus aliquid 
+    voluptates deleniti deserunt ex, odit voluptatum ad corrupti 
+    praesentium voluptatem excepturi earum modi laborum.`,
+    as: 'strong'
+  }
+}
