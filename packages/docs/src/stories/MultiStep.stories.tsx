aa2aa1dc01e519a86d2e8f58ded7293f3971ed62
Matheus Castro <goca1377@gmail.com>
2022-10-29 23:03:03 -0300

    feat: add multi step component

---
+++ packages/docs/src/stories/MultiStep.stories.tsx
@@ -0,0 +1,22 @@
+import type { StoryObj, Meta } from '@storybook/react'
+import { Box, MultiStep, MultiStepProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Form/Multi Step',
+  component: MultiStep, 
+  args: {
+    size: 4,
+    currentStep: 2
+  },
+  decorators: [
+    (Story) => {
+      return (
+        <Box as='label' css={{ display: 'flex', flexDirection: 'column', gap: '$2' }}>
+          {Story()}
+        </Box>
+      )
+    }
+  ]
+} as Meta<MultiStepProps>
+
+export const Primary: StoryObj<MultiStepProps> = {}
